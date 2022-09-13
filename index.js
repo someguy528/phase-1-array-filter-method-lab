d7ebcdf58645e2a6a629bbad3a57164a1ffdca28
Brandon Castro <castroyou999@gmail.com>
2022-09-13 17:24:44 -0400

    commit tree c986cfbdb36d4aa20e0f26244f8aa4dc7392f2b5

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+function findMatching(driverNames, unique){
+    return driverNames.filter(name => name.toUpperCase() === unique.toUpperCase())
+};
+function fuzzyMatch(driverNames, unique){
+    return driverNames.filter(name => name[0].toUpperCase() === unique[0].toUpperCase())
+};
+function matchName(driverNames, unique){
+    return driverNames.filter(driver => driver.name === unique)
+};
